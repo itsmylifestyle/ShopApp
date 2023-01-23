4a64b4b12a196dd20572c9398bf43b38a7baa936
itsmylifestyle <hahaii00@mail.ru>
2023-01-23 22:52:04 +0600

    Initial Commit

---
+++ shopApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shopApp
+//
+//  Created by Айбек on 23.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shopApp/shopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopAppApp.swift
+//  shopApp
+//
+//  Created by Айбек on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct shopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
